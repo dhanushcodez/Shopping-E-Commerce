a61bdc9289936eccda7ee40eaaf9be78f63deb01
Dhanush <radha@8522077813@gmail.com>
2024-02-02 01:42:09 +0530

    test 4

--- server.js
+++ server.js
@@ -25,6 +25,7 @@ const __dirname = path.dirname(__filename)
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
+app.use(express.static(path.join(__dirname,'./')))
 
 //routes
 app.use("/api/v1/auth", authRoutes);
@@ -32,9 +33,9 @@ app.use("/api/v1/category", categoryRoutes);
 app.use("/api/v1/product", productRoutes);
 
 //rest api
-app.get("/", (req, res) => {
-  res.send("<h1>Welcome to ecommerce app</h1>");
-});
+app.use("*",function (req,res){
+  res.sendFile(path.join(__dirname,'./build/index.html'))
+})
 
 //PORT
 const PORT = process.env.PORT || 8080;
